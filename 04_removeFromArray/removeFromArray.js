ab099591ed4cf05768491990bd0e5c2fe10300fa
Galen Schick <gschick3@gmail.com>
2022-09-24 23:53:35 -0500

    complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...n) {
+    for (let i = 0; i < array.length; i++) {
+        if (n.includes(array[i])) array.splice(i--, 1);
+    }
+    return array;
 };
 
 // Do not edit below this line
