0ed72779f2ef0b557e8c2239b2feeaa2ba70d608
Galen Schick <gschick3@gmail.com>
2022-09-24 23:35:00 -0500

    complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let newWord = "";
+    for (let i = 0; i < times; i++) {
+        newWord += word;
+    }
+    return times < 0 ? "ERROR" : newWord;
 };
 
 // Do not edit below this line
