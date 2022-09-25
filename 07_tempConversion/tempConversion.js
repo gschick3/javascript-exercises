90978e45f69a36e4373fd6852b3aba6aab88d25e
Galen Schick <gschick3@gmail.com>
2022-09-25 00:18:42 -0500

    complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
+function round(n, p) {
+  return Math.round(n * 10**p) / 10**p;
+}
 
+const ftoc = function(f) {
+  return round((f-32) * .5555, 1);
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  return round(c / .5555 + 32, 1);
 };
 
 // Do not edit below this line
